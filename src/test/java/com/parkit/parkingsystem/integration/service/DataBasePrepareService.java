935432cfb48a676ba59a69087ff0af2ed0b4bb3e
MrgnCpn <morgancpn.dev@gmail.com>
2020-04-16 01:38:17 +0200

    Write and update tests in Ticket DAO

--- src/test/java/com/parkit/parkingsystem/integration/service/DataBasePrepareService.java
+++ src/test/java/com/parkit/parkingsystem/integration/service/DataBasePrepareService.java
@@ -20,7 +20,7 @@ public class DataBasePrepareService {
             connection.prepareStatement("TRUNCATE TABLE ticket").execute();
 
             //set TEST ticket
-            connection.prepareStatement("INSERT INTO ticket VALUES (1, 1, \"TST_TCKT\", 0.50, CAST(\"2020-01-01 10:00:00\" AS DATETIME), CAST(\"2020-01-01 10:10:00\" AS DATETIME))").execute();
+            connection.prepareStatement("INSERT INTO ticket VALUES (1, 1, \"TST_TCKT\", 10, CAST(\"2020-01-01 10:00:00\" AS DATETIME), CAST(\"2020-01-01 10:10:00\" AS DATETIME))").execute();
 
         }catch(Exception e){
             e.printStackTrace();
--- src/test/java/com/parkit/parkingsystem/unit/dao/TicketDAOTest.java
+++ src/test/java/com/parkit/parkingsystem/unit/dao/TicketDAOTest.java
@@ -1,12 +1,15 @@
 package com.parkit.parkingsystem.unit.dao;
 
+import com.parkit.parkingsystem.constants.ParkingType;
 import com.parkit.parkingsystem.dao.TicketDAO;
 import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
 import com.parkit.parkingsystem.integration.service.DataBasePrepareService;
+import com.parkit.parkingsystem.model.ParkingSpot;
 import com.parkit.parkingsystem.model.Ticket;
 import org.junit.jupiter.api.*;
 
 import java.sql.Timestamp;
+import java.util.Date;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -35,31 +38,58 @@ class TicketDAOTest {
         Ticket ticket = ticketDAO.getTicket("TST_TCKT");
 
         assertThat(ticket.getId()).isEqualTo(1);
-        assertThat(ticket.getPrice()).isEqualTo(0.5);
+        assertThat(ticket.getPrice()).isEqualTo(10);
         assertThat(ticket.getInTime().getTime()).isEqualTo(Timestamp.valueOf("2020-01-01 10:00:00").getTime());
         assertThat(ticket.getOutTime().getTime()).isEqualTo(Timestamp.valueOf("2020-01-01 10:10:00").getTime());
     }
 
+    @Disabled("Plus de tests")
     @Test
-    void saveTicketReturnBoolean() {
+    void saveTicket() {
         Ticket ticket = new Ticket();
-        assertThat(ticketDAO.saveTicket(ticket)).isInstanceOf(Boolean.class);
+        ticket.setVehicleRegNumber("NEW_TCKT");
+        ticket.setPrice(10);
+        ticket.setInTime(new Date());
+        ticket.setOutTime(new Date());
+        ticket.setParkingSpot(new ParkingSpot(2, ParkingType.CAR, false));
+        ticketDAO.saveTicket(ticket);
+
+        Ticket ticketDB = ticketDAO.getTicket("NEW_TCKT");
+
+        assertThat(ticketDB.getPrice()).isEqualTo(10);
+        assertThat(ticketDB.getParkingSpot()).isEqualTo(new ParkingSpot(2, ParkingType.CAR, false));
+    }
+
+    @Disabled("A réecrire")
+    @Test
+    void updateTicketReturnBoolean() {
+        Ticket ticket = ticketDAO.getTicket("TST_TCKT");
+        ticket.setPrice(30);
+        ticket.setParkingSpot(new ParkingSpot(1, ParkingType.BIKE, false));
+        ticketDAO.updateTicket(ticket);
+        assertThat(ticketDAO.updateTicket(ticket)).isInstanceOf(Boolean.class);
+        assertThat(ticketDAO.getTicket("TST_TCKT").getPrice()).isEqualTo(30);
+        assertThat(ticketDAO.getTicket("TST_TCKT").getParkingSpot()).isEqualTo(new ParkingSpot(1, ParkingType.BIKE, false));
     }
 
     @Test
-    void updateTicketReturnBoolean() {
-        Ticket ticket = new Ticket();
+    void updateTicketWithFidelityDiscount(){
+        Ticket ticket = ticketDAO.getTicket("TST_TCKT");
+        for (int i = 0; i < 3; i ++) {
+            ticketDAO.saveTicket(ticket);
+        }
         assertThat(ticketDAO.updateTicket(ticket)).isInstanceOf(Boolean.class);
+        assertThat(ticketDAO.getTicket("TST_TCKT").getPrice()).isEqualTo(9.5);
     }
 
     @AfterEach
     public void tearDownPerTest(){
         ticketDAO = null;
+        dataBasePrepareService.clearDataBaseEntries();
     }
 
     @AfterAll
     public static void tearDown(){
-        dataBasePrepareService.clearDataBaseEntries();
         dataBasePrepareService = null;
     }
 }
